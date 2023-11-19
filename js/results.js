f587c001fbc64cb94e9303cfe2992ddf4b46e5db
Jorge Sarricolea <jjorgesarricolea18@gmail.com>
2023-11-19 17:52:28 -0600

    get the BMIScale object to find the min and max values

--- js/results.js
+++ js/results.js
@@ -25,29 +25,41 @@ elementById("bmi-text").textContent = BMI;
 
 // BMI Scale
 const BMIScale = {
-  severeThinness: { min: 0, max: 16, name: "Severe Thinness" },
-  moderateThinness: { min: 16.01, max: 17, name: "Moderate Thinness" },
-  mildThinness: { min: 17.01, max: 18.59, name: "Mild Thinness" },
-  normal: { min: 18.6, max: 25, name: "Normal" },
-  overWeight: { min: 25.01, max: 30, name: "Overweight" },
-  obeseClass1: { min: 30.01, max: 35, name: "Obese Class 1" },
-  obeseClass2: { min: 35.01, max: 40, name: "Obese Class 2" },
-  obeseClass3: { min: 40.01, max: 100, name: "Obese Class 3" },
+  severeThinness: { min: 0, max: 16, name: "Severe Thinness", color: "red" },
+  moderateThinness: {
+    min: 16.01,
+    max: 17,
+    name: "Moderate Thinness",
+    color: "orange",
+  },
+  mildThinness: {
+    min: 17.01,
+    max: 18.59,
+    name: "Mild Thinness",
+    color: "yellow",
+  },
+  normal: { min: 18.6, max: 25, name: "Normal", color: "green" },
+  overWeight: { min: 25.01, max: 30, name: "Overweight", color: "blue" },
+  obeseClass1: { min: 30.01, max: 35, name: "Obese Class 1", color: "yellow" },
+  obeseClass2: { min: 35.01, max: 40, name: "Obese Class 2", color: "orange" },
+  obeseClass3: { min: 40.01, max: 100, name: "Obese Class 3", color: "red" },
 };
 
 // Function to determine BMI category
 function getBMICategory(BMI) {
   for (var key in BMIScale) {
     if (BMI >= BMIScale[key].min && BMI <= BMIScale[key].max) {
-      return BMIScale[key].name;
+      return BMIScale[key]; // Returns the entire BMI category object
     }
   }
-  return "Undefined Category"; // In case the BMI is not in any defined category
+  return { name: "Undefined Category", min: 0, max: 0 }; // In case the BMI is not in any defined category
 }
 
-// Print the BMI category element
-const BMIcategory = getBMICategory(BMI);
-elementById("bmi-category-text").textContent = BMIcategory;
+// Print the category
+const categoryObject = getBMICategory(BMI);
+elementById(
+  "bmi-category-text"
+).textContent = `Category: ${categoryObject.name}, Range: ${categoryObject.min} - ${categoryObject.max}`;
 
 // Calculate MBR
 function CalculateMBR(age, height, weight, selectedOption) {
@@ -62,30 +74,34 @@ function CalculateMBR(age, height, weight, selectedOption) {
   }
 }
 
-// Save the MBR
+// Print the MBR element
 const MBR = CalculateMBR(age, height, weight, selectedOption);
-(elementById("mbr-text").textContent = `You need ${MBR} kcal everyday`), MBR;
+elementById("mbr-text").textContent = `You need ${MBR} kcal everyday`;
 
 // Physical activity scale
 const activityScale = {
-  sedentary: { value: 1.2 },
-  light: { value: 1.375 },
-  moderate: { value: 1.55 },
-  active: { value: 1.725 },
-  "very-active": { value: 2 },
+  sedentary: { value: 1.2, name: "Sedentary" },
+  light: { value: 1.375, name: "Light workout" },
+  moderate: { value: 1.55, name: "Moderate workout" },
+  active: { value: 1.725, name: "Active workout" },
+  "very-active": { value: 2, name: "Very active workout" },
 };
 
 // Function to determine MBR + Physical activity
 function getMBRactivity(MBR, activityLevel) {
   if (activityLevel in activityScale) {
     var physicalActivityValue = activityScale[activityLevel].value;
-    console.log(activityLevel, "| value: ", physicalActivityValue);
     return MBR * physicalActivityValue;
   } else {
     return "Undefined Physical Activity";
   }
 }
 
-// Save the MBR + Physical activity
-var MBRplusActivity = getMBRactivity(MBR, activityLevel).toFixed(2);
-console.log("MBR whit physical activity:", MBRplusActivity);
+// Print the MBR + Physical activity element
+const MBRplusActivity = getMBRactivity(MBR, activityLevel).toFixed(2);
+elementById(
+  "mbr-activity-text"
+).textContent = `Physical Activity: ${activityScale[activityLevel].name}. You need ${MBRplusActivity} kcal`;
+elementById(
+  "mbr-explanation"
+).textContent = `This value gets by multiply ${activityScale[activityLevel].value} by your current Metabolic Basal Rate.`;
